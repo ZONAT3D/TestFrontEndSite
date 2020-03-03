66a081a98366763d0138875a0ecb3a5f5f8e2464
mg70133 <groemark@johndeere.com>
2020-03-03 11:27:56 -0600

    Added using states

--- src/Components/Notification.js
+++ src/Components/Notification.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { FaRegWindowClose } from "react-icons/fa";
 import '../App.css';
 
 export class Notification extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showNoti: true
+        }
+
+        // Binds
+        this.closeNotification = this.closeNotification.bind(this)
+    }
+
     render() {
         return (
-            <div className={ 'Notification ' + this.props.notiType } >
-                <p style={{ fontWeight: "bold", minWidth: "90px" }} >{this.props.notiType}:&nbsp;</p>
-                <p>{this.props.message}</p>
-                <button onClick={closeNotification} style={{ marginRight: "15px", marginLeft: "auto", order: "2"}}>X</button>
-            </div>
+            <>
+                {this.state.showNoti && 
+                <div className={ 'Notification ' + this.props.notiType } >
+                    <p style={{ fontWeight: "bold", minWidth: "90px" }} >{this.props.notiType}:&nbsp;</p>
+                    <p>{this.props.message}</p>
+                    <FaRegWindowClose class="far" onClick={this.closeNotification} style={{ marginRight: "15px", marginLeft: "auto", order: "2"}}></FaRegWindowClose>
+                </div>
+                }
+            </>
         );
     }
-}
 
-function closeNotification(button) {
-    button.target.parentNode.style = "display: none;";
+    // Handles the closing of Notifications
+    closeNotification = function() {
+        this.setState({ showNoti: false });
+    }
 }
